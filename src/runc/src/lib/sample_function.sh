2195570738f995a61c07d687c67f153950166420
Marcus Flyckt <marcus.flyckt@gmail.com>
2021-11-26 20:56:26 +0100

    Turned runc into a bashly script

---
+++ src/runc/src/lib/sample_function.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+## Add any function here that is needed in more than one parts of your
+## application, or that you otherwise wish to extract from the main function
+## scripts.
+##
+## Note that code here should be wrapped inside bash functions, and it is
+## recommended to have a separate file for each function.
+##
+## Subdirectories will also be scanned for *.sh, so you have no reason not
+## to organize your code neatly.
+##
+generate_c_program() {
+  local code="${1?No code}"
+  cat <<-EOF
+    #include <stdio.h>
+    #include <stdlib.h>
+
+    int main(int argc, char *argv[])
+    {
+      $code
+    }
+EOF
+}
---
+++ src/runc/src/root_command.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -eu
+
+TEMP_FILE="$(mktemp)"
+TEMP_EXECUTABLE="$(mktemp)"
+
+finish() {
+	rm -f "$TEMP_FILE"
+	rm -f "$TEMP_EXECUTABLE"
+}
+
+trap finish EXIT
+
+CODE="$(cat)"
+
+generate_c_program "$CODE" > "$TEMP_FILE"
+
+gcc -xc "$TEMP_FILE" -o "$TEMP_EXECUTABLE"
+
+"$TEMP_EXECUTABLE"
+
